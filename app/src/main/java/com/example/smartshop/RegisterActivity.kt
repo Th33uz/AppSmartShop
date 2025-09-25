2dd3a1397284f22ef2fcf116d7b37afc939afb52
Th33uz <matheus-zika101@live.com>
2025-09-25 15:38:12 -0300

    Register user, e verificações

--- app/src/main/java/com/example/smartshop/RegisterActivity.kt
+++ app/src/main/java/com/example/smartshop/RegisterActivity.kt
@@ -1,11 +1,53 @@
 package com.example.smartshop
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.smartshop.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegistrarFinal.setOnClickListener {
+            val nome = binding.inputNome.text.toString()
+            val email = binding.inputEmailRegister.text.toString()
+            val senha = binding.inputSenhaRegister.text.toString()
+            val confirmaSenha = binding.inputConfirmaSenha.text.toString()
+
+
+            if (nome.isEmpty() || email.isEmpty() || senha.isEmpty() || confirmaSenha.isEmpty()) {
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (senha != confirmaSenha) {
+                Toast.makeText(this, "As senhas não coincidem", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+
+            if (UserSession.usuarios.any { it.email == email }) {
+                Toast.makeText(this, "Já existe um usuário com este e-mail", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+                // lembrando esse return @setonClickListener e para sair do click e voltar para onCreate
+            }
+
+            val novoUsuario = User(nome, email, senha)
+            UserSession.usuarios.add(novoUsuario)
+
+            Toast.makeText(this, "Usuário registrado com sucesso!", Toast.LENGTH_SHORT).show()
+
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
